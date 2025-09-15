fba18b33fc1b4f902f8814d4a00787a266305437
Mathias Jave Diaz <u202212721@upc.edu.pe>
2025-09-15 00:45:08 -0500

    fix: encapzulated formating operations inside try catch

--- src/utils/formatters.py
+++ src/utils/formatters.py
@@ -1,19 +1,19 @@
 def price_formatter(price) -> float:
-    patterns = "|,₹"
-    for pattern in patterns:
-        if pattern in price:
-            price = price.replace(pattern, "")
     try:
-        return float(price)
+        patterns = "|,₹"
+        for pattern in patterns:
+            if pattern in price:
+                price = price.replace(pattern, "")
+            return float(price)
     except ValueError:
         return 0.0
 
 def ratting_formatter(ratting) -> float:
-    if "," in ratting:
-        ratting = ratting.replace(",", ".")
-    if "|" in ratting:
-        ratting = ratting.replace("|", "")
     try:
+        if "," in ratting:
+            ratting = ratting.replace(",", ".")
+        if "|" in ratting:
+            ratting = ratting.replace("|", "")
         return float(ratting)
     except ValueError:
         return 0.0
